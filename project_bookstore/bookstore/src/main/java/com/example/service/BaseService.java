4110fb49e12b7e89920381bc3f44824c077d1262
xl Deer <YAOXL819@outlook.com>
2020-04-04 22:07:39 +0800

    delete bookstore -Spring+Spring MVC+MyBatis

--- project_bookstore/bookstore/src/main/java/com/example/service/BaseService.java
+++
@@ -1,5 +0,0 @@
-package com.example.service;
-
-public interface BaseService<T> {
-    void add(T t) throws Exception;
-}
--- project_bookstore/bookstore/src/main/java/com/example/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.service;
-
-import com.example.domain.User;
-
-public interface UserService extends BaseService<User> {
-    void add(User user) throws Exception;
-}
